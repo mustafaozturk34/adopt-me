3d00bf5f2a98af1fc6992d181a3e39f371463969
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-04-10 15:27:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt me</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
