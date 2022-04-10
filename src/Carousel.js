58c0c010dfeb722c80baaa74e877c3b44b75f5d8
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-04-10 15:50:35 +0300

    interactive class component

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,41 @@
 import { Component } from "react";
 
 class Carousel extends Component {
-    state = {
-        active: 0
-    };
+  state = {
+    active: 0,
+  };
 
-    static defaultProps = {
-        images: ['http:pets-images.dev-apis.com/pets/none.jpg']
-    }
+  static defaultProps = {
+    images: ["http:pets-images.dev-apis.com/pets/none.jpg"],
+  };
 
-    render() {
-        const { active } = this.state;
-        const { images } = this.props;
+  handleIndexClick = (event) => {
+    this.setState({
+        active: +event.target.dataset.index
+    });
+  }
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
 
-        return(
-            <div className="carousel">
-                <img src={images[active]} alt="animal" />
-                <div className="carousel-smaller">
-                    {images.map((photo, index) => (
-                        <img 
-                            key={photo}
-                            src={photo}
-                            className={index === active ? "active" : ""}
-                            alt="animal thumbnail"
-                        />
-                    ))}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
