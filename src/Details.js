8169a8a73b286a7c96a3497d48dff9eaa577c7ff
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-04-10 16:39:43 +0300

    error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
- 
+
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -20,13 +21,14 @@ class Details extends Component {
     );
   }
   render() {
-    if(this.state.loading){
-      return <h2>loading...</h2>
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
     }
-    const { animal, breed, city, state, description, name, images } = this.state;
-    return(
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
       <div className="details">
-        <Carousel images={images}/>
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city},${state}`}</h2>
@@ -34,8 +36,16 @@ class Details extends Component {
           <p>{description}</p>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
--- src/Pet.js
+++ src/Pet.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
